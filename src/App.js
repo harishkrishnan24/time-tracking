a0e4393a22db7f6bc100ff6e87e16ca68afe5943
Harish Krishnan <harishkrishnan1993@gmail.com>
2018-01-23 17:35:48 +0530

    Basic structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import client from './client';
+import helpers from './helper';
+
+class TimersDashboard extends React.Component {
+  render() {
+    return();
+  }
+}
+
+export default TimersDashboard;
